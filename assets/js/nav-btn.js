5af72531b42bc5071d12e4e1df3be904b7fa1501
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-04-23 13:54:35 +0100

    Adjusted nav-btn.js
    Added a new function which allows for a mobile menu overlay to occur without interfering with the normal menu, may need to look at stopping normal menu from dropping down in bs studio

--- assets/js/nav-btn.js
+++ assets/js/nav-btn.js
@@ -16,4 +16,21 @@
             $(y).removeClass('fixed-top');
         }
     });
+    $("#mobile-overlay").click(
+    function mobileNavBtn(){
+        document.getElementById('mobile-overlay').classList.toggle("change");
+        var x = document.getElementById('myNav');
+        var y = document.getElementById('main-nav');
+        if(x.style.height === "100%") {
+            x.style.height ="0%";
+            $('.main').removeClass('active');
+            $("body").removeClass('noscroll');
+            $(y).addClass('fixed-top');
+        } else {
+            x.style.height = "100%";
+            $('.main').addClass('active');
+            $("body").addClass('noscroll');
+            $(y).removeClass('fixed-top');
+        }
+    });
 })(jQuery);
