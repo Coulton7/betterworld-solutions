b6e8f56b38b9eaed12c6645402e6b09e9b058170
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-04-24 15:11:03 +0100

    Uploading changes to the files

--- assets/js/form-validate.js
+++ assets/js/form-validate.js
@@ -9,37 +9,49 @@ $(document).ready(function() {
     var type1 = document.getElementById('type1');
     var type2 = document.getElementById('type2');
     var type3 = document.getElementById('type3');
+    var error = document.getElementById('error');
 
     if (orgName.value == "") {
       $(orgName).addClass("is-invalid");
+      $(error).show();  
       return false;
     }
 
     if (orgEmail.value == "") {
+      $(orgName).removeClass("is-invalid");
       $(orgEmail).addClass("is-invalid");
+      $(error).show();  
       return false;
     }
 
     if (orgPhone.value == "") {
+      $(orgEmail).removeClass("is-invalid");
       $(orgPhone).addClass("is-invalid");
+      $(error).show(); 
       return false;
     }
 
     if (orgWorkers.value == "") {
+      $(orgPhone).removeClass("is-invalid");  
       $(orgWorkers).addClass("is-invalid");
+      $(error).show(); 
       return false;
     }
 
     if (roi.value == "") {
+      $(orgWorkers).removeClass("is-invalid");
       $(roi).addClass("is-invalid");
+      $(error).show(); 
       return false;
     }
 
     if ($('input[name=orgType]:checked').length == 0) {
       window.alert("Please select a business type");
+      $(roi).removeClass("is-invalid");
       $(type1).addClass("is-invalid");
       $(type2).addClass("is-invalid");
       $(type3).addClass("is-invalid");
+      $(error).show(); 
       return false;
     } else {
       $('#createPDF').hide();
