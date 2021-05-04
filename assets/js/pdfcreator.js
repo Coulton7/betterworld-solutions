b7d76a3d5a9a3040e1c0d65ebdc355ee6097f338
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-04 11:44:16 +0100

    Uploading new wording changes to the file

--- assets/js/pdfcreator.js
+++ assets/js/pdfcreator.js
@@ -20,8 +20,8 @@ function pdfCreator() {
   var docDefinition = {
 
     info: {
-      title: 'POLICY TO PREVENT GLOBAL WARMING',
-      subject: 'POLICY TO PREVENT GLOBAL WARMING'
+      title: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING',
+      subject: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING'
     },
 
     pageMargins: [45, 105, 45, 60],
@@ -35,7 +35,7 @@ function pdfCreator() {
     },
 
     content: [{
-        text: 'POLICY TO PREVENT GLOBAL WARMING\n\n',
+        text: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING\n\n',
         style: 'header'
       },
       {
@@ -112,7 +112,7 @@ function pdfCreator() {
   var pdf = pdfMake.createPdf(docDefinition).download('Policy-to-prevent-Global-Warming.pdf');
 }
 
-function pdfCreator() {
+function pdfMake() {
   var orgName = document.getElementById('orgName');
   var orgPhone = document.getElementById('orgPhone');
   var orgEmail = document.getElementById('orgEmail');
@@ -141,8 +141,8 @@ if(date.value ===""){
   var docDefinition = {
 
     info: {
-      title: 'POLICY TO PREVENT GLOBAL WARMING',
-      subject: 'POLICY TO PREVENT GLOBAL WARMING'
+      title: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING',
+      subject: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING'
     },
 
     pageMargins: [45, 105, 45, 60],
@@ -156,7 +156,7 @@ if(date.value ===""){
     },
 
     content: [{
-        text: 'POLICY TO PREVENT GLOBAL WARMING\n\n',
+        text: 'INVESTMENT POLICY TO PREVENT GLOBAL WARMING\n\n',
         style: 'header'
       },
       {
